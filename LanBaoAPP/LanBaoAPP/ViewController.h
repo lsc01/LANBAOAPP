8dc97359add6a3a50accbfa267350b7796870c28
lsc <942776524@qq.com>
2018-02-27 22:24:29 +0800

    建立工程

---
+++ LanBaoAPP/LanBaoAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LanBaoAPP
+//
+//  Created by lsc on 2018/2/27.
+//  Copyright © 2018年 lsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LanBaoAPP/LanBaoAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LanBaoAPP
+//
+//  Created by lsc on 2018/2/27.
+//  Copyright © 2018年 lsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

b5871f63e35e3725152d99384095f66b7208b347
lanshichang <942776524@qq.com>
2018-03-03 17:59:31 +0800

    提交

--- LanBaoAPP/LanBaoAPP/AppDelegate.m
+++ LanBaoAPP/LanBaoAPP/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [NSURL URLWithString:@"http://www.baidu.com"];
+    
     return YES;
 }
 
